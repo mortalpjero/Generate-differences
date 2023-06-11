212dac140c453e71b1f3307f8e512e143d144784
mortalpjero <mortalpjero@gmail.com>
2023-06-11 14:54:33 -0700

    feat: added call with arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,7 @@ program
     .name('gendiff')
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
 program.parse();
