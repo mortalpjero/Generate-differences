f1f07436eae13d55ad254b503b16ecdfa853a3c8
mortal pjero <mortalpjero@gmail.com>
2023-06-28 14:35:53 +0100

    fix: changed if model to switch model

--- src/construct.js
+++ src/construct.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
+// This code is comparing two objects and makes a tree of results.
+// The item of this tree has 'key', 'type', 'value'.
+// If the 'value' is not equal to 'nested', that means, that it is a leaf of
+
 const construct = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
@@ -7,23 +11,26 @@ const construct = (obj1, obj2) => {
   const sortedValues = _.sortBy(uniqueKeys);
 
   const result = sortedValues.map((key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      return { key, type: 'added', value: obj2[key] };
-    } if (!Object.hasOwn(obj2, key)) {
-      return { key, type: 'deleted', value: obj1[key] };
-    } if (obj1[key] === obj2[key]) {
-      return { key, type: 'unchanged', value: obj1[key] };
-    } if (!_.isPlainObject(obj1[key]) || !_.isPlainObject(obj2[key])) {
-      return {
-        key,
-        type: 'changed',
-        from: obj1[key],
-        to: obj2[key],
-      };
-    }
+    let childResult; // Declare the variable here
 
-    const children = construct(obj1[key], obj2[key]);
-    return { key, type: 'nested', children };
+    switch (true) {
+      case !Object.hasOwn(obj1, key):
+        return { key, type: 'added', value: obj2[key] };
+      case !Object.hasOwn(obj2, key):
+        return { key, type: 'deleted', value: obj1[key] };
+      case obj1[key] === obj2[key]:
+        return { key, type: 'unchanged', value: obj1[key] };
+      case !_.isPlainObject(obj1[key]) || !_.isPlainObject(obj2[key]):
+        return {
+          key,
+          type: 'changed',
+          from: obj1[key],
+          to: obj2[key],
+        };
+      default:
+        childResult = construct(obj1[key], obj2[key]); // Assign the value here
+        return { key, type: 'nested', children: childResult };
+    }
   });
 
   return result;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 import { getKey, getValue, getType } from '../utils.js';
 
+// Stringify function is returning the value if it is not an object.
+// If the value is an object, it uses recursive calls to process it.
+
 const stringify = (value, currentDepth) => {
   const iter = (currentValue, depth) => {
     if (!_.isObject(currentValue)) {
@@ -18,35 +21,40 @@ const stringify = (value, currentDepth) => {
   return iter(value, currentDepth);
 };
 
+// stylish function takes our previously constructed tree
+// and processes it, to give it right iteration and build a desired output tree.
+// The indentation is calculated by keeping track of depth of the tree.
+
 const stylish = (construct) => {
   const iter = (currentValue, depth) => {
     const replacer = ' ';
     const spaceCountIter = 4;
-    const indent = replacer.repeat(depth * spaceCountIter - 2);
-    const bracketIndent = replacer.repeat((depth - 1) * spaceCountIter);
+    const indent = replacer.repeat((depth * spaceCountIter) - 2);
+    const bracketIndent = replacer.repeat(spaceCountIter * (depth - 1));
 
     const content = currentValue.map((constructed) => {
       const [key, type, value] = [getKey(constructed), getType(constructed), getValue(constructed)];
-      const valueString = stringify(value, depth + 1);
-
-      if (type === 'added') {
-        return `${indent}+ ${key}: ${valueString}`;
+      // If the types are simple,such as 'added', 'deleted', 'changed' or 'unchenged'.
+      // the output is added to the final tree.
+      // If the output is complex (the value of a key has multiple values in it)
+      // the Iter function is recursively called.
+      switch (type) {
+        case 'added':
+          return `${indent}+ ${key}: ${stringify(value, depth + 1)}`;
+        case 'deleted':
+          return `${indent}- ${key}: ${stringify(value, depth + 1)}`;
+        case 'unchanged':
+          return `${indent}  ${key}: ${stringify(value, depth + 1)}`;
+        case 'changed':
+          return [
+            `${indent}- ${key}: ${stringify(value[0], depth + 1)}`,
+            `${indent}+ ${key}: ${stringify(value[1], depth + 1)}`,
+          ].join('\n');
+        case 'nested':
+          return `${indent}  ${key}: ${iter(value, depth + 1)}`;
+        default:
+          return null;
       }
-      if (type === 'deleted') {
-        return `${indent}- ${key}: ${valueString}`;
-      }
-      if (type === 'unchanged') {
-        return `${indent}  ${key}: ${valueString}`;
-      }
-      if (type === 'changed') {
-        const [oldValue, newValue] = value;
-        return `${indent}- ${key}: ${stringify(oldValue, depth + 1)}\n${indent}+ ${key}: ${stringify(newValue, depth + 1)}`;
-      }
-      if (type === 'nested') {
-        return `${indent}  ${key}: ${iter(value, depth + 1)}`;
-      }
-
-      return null;
     });
 
     return ['{', ...content, `${bracketIndent}}`].join('\n');
