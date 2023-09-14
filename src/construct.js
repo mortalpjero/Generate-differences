c9312ffa0b58e687fc2028886798aea90eaacea3
mortal pjero <mortalpjero@gmail.com>
2023-09-14 06:50:34 +0100

    fix: refactored construct

--- src/construct.js
+++ src/construct.js
@@ -11,23 +11,24 @@ const construct = (obj1, obj2) => {
   const sortedValues = _.sortBy(uniqueKeys);
 
   const result = sortedValues.map((key) => {
-    switch (true) {
-      case !Object.hasOwn(obj1, key):
-        return { key, type: 'added', value: obj2[key] };
-      case !Object.hasOwn(obj2, key):
-        return { key, type: 'deleted', value: obj1[key] };
-      case obj1[key] === obj2[key]:
-        return { key, type: 'unchanged', value: obj1[key] };
-      case !_.isPlainObject(obj1[key]) || !_.isPlainObject(obj2[key]):
-        return {
-          key,
-          type: 'changed',
-          from: obj1[key],
-          to: obj2[key],
-        };
-      default:
-        return { key, type: 'nested', children: construct(obj1[key], obj2[key]) };
+    if (!Object.hasOwn(obj1, key)) {
+      return { key, type: 'added', value: obj2[key] };
     }
+    if (!Object.hasOwn(obj2, key)) {
+      return { key, type: 'deleted', value: obj1[key] };
+    }
+    if (obj1[key] === obj2[key]) {
+      return { key, type: 'unchanged', value: obj1[key] };
+    }
+    if (!_.isPlainObject(obj1[key]) || !_.isPlainObject(obj2[key])) {
+      return {
+        key,
+        type: 'changed',
+        from: obj1[key],
+        to: obj2[key],
+      };
+    }
+    return { key, type: 'nested', children: construct(obj1[key], obj2[key]) };
   });
 
   return result;
